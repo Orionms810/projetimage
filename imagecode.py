70fa8bfd63f7ecac8541a6d9bf6070d9eadc3dd8
Orionms810 <milos.dj007@gmail.com>
2024-01-06 23:12:13 +0100

    ajouts defintion test dissimuler et extraire en un + def. dissimuler message +def extraire message avec le if__name__ == "__main__":

--- imagecode.py
+++ imagecode.py
@@ -12,3 +12,75 @@ def generer_nom_image_contenant_message(image_path):
     return message_image_name
 
 
+def test_dissimuler_extraire_message():
+    
+    original_image_path = "path/vers/votre/image_temporaire.png"
+    Image.new("RGB", (500, 500)).save(original_image_path)
+
+    message = "dissimulation et extraction du message."
+
+    message_image_path = dissimuler_message(original_image_path, message)
+
+    assert os.path.isfile(message_image_path)
+
+    extracted_message = extraire_message(message_image_path)
+    assert extracted_message == message
+    
+    os.remove(original_image_path)
+    os.remove(message_image_path)
+    
+def dissimuler_message(image_path, message, message_image_path=None):
+        
+    original_image = Image.open(image_path)
+    width, height = original_image.size
+
+    binary_message = ''.join(format(ord(char), '08b') for char in message)
+
+    steg_image = original_image.copy()
+
+    index = 0 
+
+    for y in range(height):
+        for x in range(width):
+            pixel = list(steg_image.getpixel((x, y)))
+
+            for i in range(3): 
+                if index < len(binary_message):
+                    pixel[i] = int(format(pixel[i], '08b')[:-1] + binary_message[index], 2)
+                    index += 1
+
+            steg_image.putpixel((x, y), tuple(pixel))
+
+    if message_image_path is None:
+        message_image_path = generer_nom_image_contenant_message(image_path)
+        
+    steg_image.save(message_image_path)
+
+    return message_image_path
+ 
+
+def extraire_message(image_path):
+    steg_image = Image.open(image_path)
+    width, height = steg_image.size
+
+    binary_message = ''
+    for y in range(height):
+        for x in range(width):
+            pixel = list(steg_image.getpixel((x, y)))
+
+            for i in range(3):
+                binary_message += format(pixel[i], '08b')[-1]
+    extracted_message = ''.join(chr(int(binary_message[i:i + 8], 2)) for i in range(0, len(binary_message), 8))
+
+    return extracted_message
+
+if __name__ == "__main__":
+    user_message = input("Entrez le message à dissimuler : ")
+    user_image_path = input("Entrez le chemin de l'image d'origine : ")
+    user_output_path = input("Entrez le chemin de sortie pour l'image contenant le message: ")
+
+    message_image_path = dissimuler_message(user_image_path, user_message, user_output_path)
+    print(f"Message dissimulé dans l'image : {message_image_path}")
+
+    extracted_message = extraire_message(message_image_path)
+    print(f"Message extrait : {extracted_message}")
