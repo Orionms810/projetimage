10ece980699dc6f19e32466d0c6ee2a481782b97
Orionms810 <milos.dj007@gmail.com>
2024-01-06 16:20:30 +0100

    ajout defintion générer image + fonction test

--- imagecode.py
+++ imagecode.py
@@ -1,5 +1,14 @@
 from PIL import Image
+import os
 
-monimage = Image.open("Cars.png")
+def test_generer_nom_image_contenant_message():
+    image_path = "path/vers/votre/image.png"
+    generated_name = generer_nom_image_contenant_message(image_path)
+    assert generated_name == "image_message.png"
+
+def generer_nom_image_contenant_message(image_path):
+    base_name = os.path.splitext(os.path.basename(image_path))[0]
+    message_image_name = f"{base_name}_message.png"
+    return message_image_name
 
 
